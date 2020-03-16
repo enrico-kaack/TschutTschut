645eb471cf4c0c3fe3615617ab5f98dc1377f05d
enrico-kaack <e.kaack@live.de>
2020-03-16 18:56:36 +0100

    Added: vuetify and home site

--- tschuttschut/src/views/Home.vue
+++ tschuttschut/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container class="home">
+    <h1>Tschut Tschut</h1>
+    <div>To start a new game, press the button below. To join an existing game, visit the url the host shared with you.</div>
+    <router-link to="CreateSession">
+      <v-btn>Start Game</v-btn>
+    </router-link>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
